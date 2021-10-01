2a3ed7d941d51fcc0ee9e585d4fcf71ef8151b31
Alex <ruppincs28@gmail.com>
2021-10-01 12:35:26 +0300

    Use different thread for file operations (write / close)

--- src/Pages/RegisterPage.java
+++ src/Pages/RegisterPage.java
@@ -14,7 +14,7 @@ import java.time.YearMonth;
 import java.util.Calendar;
 import java.util.Date;
 
-public class RegisterPage extends JFrame{
+public class RegisterPage extends JFrame {
     private JPanel RegisterPanel;
     private JTextField NameFiled;
     private JTextField EmailField;
@@ -26,41 +26,44 @@ public class RegisterPage extends JFrame{
     private JButton RegisterButton;
     private JTextField IdField;
 
-    public RegisterPage(String title){
+    public RegisterPage(String title) {
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(500,360));
+        this.setPreferredSize(new Dimension(500, 360));
         this.setContentPane(RegisterPanel);
 
         MonthCB.addItem("Month");
-        for (int i = 1;i<=12;i++){
-             MonthCB.addItem(i);
+        for (int i = 1; i <= 12; i++) {
+            MonthCB.addItem(i);
         }
         YearCB.addItem("Year");
-        for(int i =1900;i<= Calendar.getInstance().get(Calendar.YEAR);i++){
+        for (int i = 1900; i <= Calendar.getInstance().get(Calendar.YEAR); i++) {
             YearCB.addItem(i);
         }
         YearMonth yearMonthObject = YearMonth.of(1900, 1);
         int daysInMonth = yearMonthObject.lengthOfMonth();
         DayCB.addItem("Day");
-        for (int i =1;i<=daysInMonth;i++){
+        for (int i = 1; i <= daysInMonth; i++) {
             DayCB.addItem(i);
         }
         this.pack();
 
         RegisterButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)  {
-                try {
-                Trainee trainee = new Trainee(Integer.parseInt(IdField.getText()), NameFiled.getText(), EmailField.getText() ,PhoneField.getText(),new Date(Integer.parseInt(YearCB.getSelectedItem().toString()),Integer.parseInt(MonthCB.getSelectedItem().toString()),Integer.parseInt(DayCB.getSelectedItem().toString())),String.valueOf(passwordPasswordField.getPassword()));
-                FileWriter myWriter = new FileWriter("src/Files/Trainees.txt",true);
-                myWriter.write("\n"+trainee.toString());
-                myWriter.close();
-                System.out.println("Successfully wrote to the file.");
-                }catch (IOException ex) {
-                    System.out.println("An error occurred.");
-                    ex.printStackTrace();
-                }
+            public void actionPerformed(ActionEvent e) {
+                Runnable asyncFileOperation = () -> {
+                    try {
+                        Trainee trainee = new Trainee(Integer.parseInt(IdField.getText()), NameFiled.getText(), EmailField.getText(), PhoneField.getText(), new Date(Integer.parseInt(YearCB.getSelectedItem().toString()), Integer.parseInt(MonthCB.getSelectedItem().toString()), Integer.parseInt(DayCB.getSelectedItem().toString())), String.valueOf(passwordPasswordField.getPassword()));
+                        FileWriter myWriter = new FileWriter("src/Files/Trainees.txt", true);
+                        myWriter.write("\n" + trainee.toString());
+                        myWriter.close();
+                        System.out.println("Successfully wrote to the file.");
+                    } catch (IOException ex) {
+                        System.out.println("An error occurred.");
+                        ex.printStackTrace();
+                    }
+                };
+                new Thread(asyncFileOperation).start();
             }
         });
         MonthCB.addItemListener(new ItemListener() {
@@ -69,7 +72,7 @@ public class RegisterPage extends JFrame{
                 YearMonth yearMonthObject = YearMonth.of(1900, Integer.parseInt(MonthCB.getSelectedItem().toString()));
                 int daysInMonth = yearMonthObject.lengthOfMonth();
                 DayCB.removeAllItems();
-                for (int i =1;i<=daysInMonth;i++){
+                for (int i = 1; i <= daysInMonth; i++) {
                     DayCB.addItem(i);
                 }
             }
